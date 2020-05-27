d8fd7d49e8cf4ba84a1ce3b4e729d5cfc99b3fee
energetic-course-5565 <cary.l.williams00@gmail.com>
2020-05-27 19:41:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,8 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language (name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language ("Josh", "Ruby")
+introduction_with_language("Josh", "Ruby")
